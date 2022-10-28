d585a544e928eb124df4d88fc7c35c07223524bc
dianawambuim <dianawambuim@gmail.com>
2022-10-29 02:28:16 +0300

    butoo n function added

--- akan.js
+++ akan.js
@@ -2,4 +2,10 @@ function submitFunction()
 {
   //check on date selected
   //check on gender selected
+};
+function getElementById()
+{
+  const dateOfBirthElement = document.getElementById('dateOfBirth');
+  const genderElement = document.getElementById('rmale','rfemale');
+  return{dateOfBirth,rmale,rfemale}
 }
