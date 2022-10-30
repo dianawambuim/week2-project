7ef54dd033fbd65dbc3f1dbcee63f459c54b6e8a
dianawambuim <dianawambuim@gmail.com>
2022-10-30 23:58:16 +0300

    corrected on submit function

---
+++ index.js
@@ -0,0 +1,53 @@
+function submitFunction() {
+//document.getElementById("submit").addEventListener = (.id);
+document.getElementById("submit").innerHTML = "Your Akan name is...!";
+document.getElementById("submit").innerHTML = style.color = "blue";
+};
+
+// //check on date selected
+// dateOfBirthchecker(pageValues.dateOfBirthValue);
+// //check on gender selected
+// //get page elements
+// const pageElements = getElements();
+// const pageValues = getValuesFromElements(pageElements);
+// };
+//
+// function getElementById() {
+//   const dateOfBirthElement = document.getElementById('dateOfBirth')
+// };
+// const rmaleElement = document.getElementById('rmale');
+// const rfemaleElement = document.getElementById('rfemale');
+// return {
+//   dateOfBirthElement,
+//   rmaleElement,
+//   rfemaleElement
+// };
+// };
+//
+// function getValuesFromElements(input) {
+//   const dateOfBirthElement = input.dateOfBirthElement;
+//   const rmaleElement = input.rmaleElement;
+//   const rfemaleElement = input.rfemaleElement;
+//
+//   const dateOfBirthValue = dateOfBirthElement.value;
+//   const rmaleValue = rmaleElement.value;
+//   const rfemaleValue = rfemaleElement.value;
+//   return {
+//     dateOfBirthValue,
+//     rmaleValue,
+//     rfemaleValue
+//   }
+//
+// };
+//
+// function dateOfBirthchecker(dateOfBirth) {
+//   if (input.value == "") {
+//     alert("Select date of birth")
+//   }
+//
+//
+//   const dayOfWeek = Result of the formula;
+//
+//   const maleNames[Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
+//   const femaleNames[Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
+//   //  const myName[dayOfWeek - 1]
